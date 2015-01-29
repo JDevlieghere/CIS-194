8b0d1c48aeb06240bbc5495282267bde24843be7
JDevlieghere <info@jonasdevlieghere.com>
2015-01-29 21:58:18 +0100

    Added week 4

---
+++ Week 4/Wholemeal.hs
@@ -0,0 +1,8 @@
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+    | even x = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = foldr (\x y -> if even x then (x - 2) * y else y) 1
